925a21397d61b4e7522dcb7d7176bc557630076a
Anass Kerchaoui <anass.kerchaoui@efrei.net>
2025-06-03 15:13:09 +0200

    init rabbitmq

--- public_api/src/app.module.ts
+++ public_api/src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'OCR_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://user:password@rabbitmq:5672'],
+          queue: 'ocr_queue',
+          queueOptions: { durable: false },
+        },
+      },
+      {
+        name: 'TEXT_TREATMENT_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://user:password@rabbitmq:5672'],
+          queue: 'text_treatment_queue',
+          queueOptions: { durable: false },
+        },
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
