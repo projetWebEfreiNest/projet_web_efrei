5e31ff23d05b0ceca4709e63365697199a35fe7f
cmoileo <leo.fezard33@gmail.com>
2025-06-16 14:55:44 +0200

    fix cors error

--- public_api/src/main.ts
+++ public_api/src/main.ts
@@ -3,6 +3,21 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: [
+      'Content-Type',
+      'Authorization',
+      'Accept',
+      'X-Requested-With',
+    ],
+    credentials: true,
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
+
   await app.listen(3000);
 }
 bootstrap();
